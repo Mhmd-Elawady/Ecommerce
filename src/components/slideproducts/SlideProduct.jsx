65dccce77378221de527579fb97d97e7053ba17a
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2026-03-16 01:19:02 +0200

    Improve responsive layout in CSS

--- src/components/slideproducts/SlideProduct.jsx
+++ src/components/slideproducts/SlideProduct.jsx
@@ -16,11 +16,12 @@ function SlideProduct({ data, title }) {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2500,
-    responsive: [
-      { breakpoint: 1024, settings: { slidesToShow: 4 } },
-      { breakpoint: 768, settings: { slidesToShow: 2 } },
-      { breakpoint: 480, settings: { slidesToShow: 1 } },
-    ]
+  responsive: [
+  { breakpoint: 1200, settings: { slidesToShow: 4 } },
+  { breakpoint: 1024, settings: { slidesToShow: 3 } },
+  { breakpoint: 768,  settings: { slidesToShow: 2 } },
+  { breakpoint: 480,  settings: { slidesToShow: 1, arrows: false } },
+]
   };
 
   return (
