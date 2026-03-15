972c828d935d52bef8935c1c37c0f275d02efa07
Mhmd-Elawady <mhmdelmetwally22@gmail.com>
2026-03-15 16:54:45 +0200

    update project

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import CartProvider from './components/Context/CartContext.jsx'
 import AuthProvider from './components/Context/AuthContext.jsx'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename="/Ecommerce">
       <AuthProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
